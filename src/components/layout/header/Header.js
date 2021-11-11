e2f67378bfa69b87381eae9ff4c9feafab57597d
Isela <isela.v.alarcon@gmail.com>
2021-11-11 17:36:38 +0100

    creating a new react challenge

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//IMPORT STYLES
+import { main } from "./Header.styles";
+
+const Header = () => {
+  return <main>alo</main>;
+};
+
+export default Header;
---
+++ src/components/layout/header/Header.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const main = styled.main`
+  font-size: 16px;
+  color: gray;
+`;
