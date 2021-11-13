46d61092fed78dc9000fa8fcd4ce11325c1c60f3
Isela <isela.v.alarcon@gmail.com>
2021-11-13 16:28:02 +0100

    setting the buttons

--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -1,1 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
+//IMPORT COMPONENTS
+import CreateAccount from "../../elements/CreateAccount/CreateAccount";
+
+//IMPORT STYLES
+import { InputBar, MainContainer } from "./Main.styles";
+
+const SearchBar = () => {
+  return (
+    <main>
+      <MainContainer>
+        <InputBar
+          type="text"
+          placeholder="Search for artist, bands, tracks, podcast"
+        ></InputBar>
+        <p>or</p>
+        <CreateAccount />
+      </MainContainer>
+    </main>
+  );
+};
+
+export default SearchBar;
