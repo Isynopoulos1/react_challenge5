5740cc0bb780286cc078b8e810a17725fd8facf4
Isela <isela.v.alarcon@gmail.com>
2021-11-13 18:11:44 +0100

    stting the search bar part 2

--- src/components/elements/SearchBar/SearchBar.styles.js
+++ src/components/elements/SearchBar/SearchBar.styles.js
@@ -17,8 +17,10 @@ export const InputBar = styled.input`
   background-color: #eee;
   border-radius: 4px;
   margin-bottom: 20px;
-  font-size: 18px;
-  padding: 0 300px 0px 10px;
+  font-size: 16px;
+  width: 500px;
+  color: #999999;
+  padding-left: 10px;
 `;
 
 export const InputWrapper = styled.div`
@@ -32,13 +34,10 @@ export const Text = styled.p`
 `;
 
 export const IconStyle = styled.span`
-  right: 0;
-  top: 35px;
-
+  right: 10px;
+  top: 30px;
   position: absolute;
   font-family: "Material Icons";
-  font-size: 20px;
-  color: #333;
-  margin-right: 3px;
-  margin-left: 10px;
+  font-size: 30px;
+  color: #666666;
 `;
