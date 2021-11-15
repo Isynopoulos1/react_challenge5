0d11cdf38f479fd085310dba5a3201d243130b74
Isela <isela.v.alarcon@gmail.com>
2021-11-15 22:05:34 +0100

    setting the map list part 2

--- src/components/data/cards.js
+++ src/components/data/cards.js
@@ -1,73 +1,73 @@
 export const list = [
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.53.46_7LssM184rk.png?updatedAt=1636901796501",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.53.54_gZ7XqRa7w-.png?updatedAt=1636901806952",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.01_PcxUL-xru5.png?updatedAt=1636901789605",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.08_4NQQ6zvHB.png?updatedAt=1636901801903",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.08_4NQQ6zvHB.png?updatedAt=1636901801903",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.15_JMpZY6pX4M.png?updatedAt=1636901820564",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.23_6TCwv7t0m-.png?updatedAt=1636901813712",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.39_CJxfb3SDVc.png?updatedAt=1636901854846",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.33_2PgPk3EEt8.png?updatedAt=1636901869272",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.46_2ssXpbmfBT.png?updatedAt=1636901830504",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.55.02_XmmZx1-9f.png?updatedAt=1636901861974",
     title: "lalala",
     album: "lololo"
   },
   {
     img:
-      "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
+      "https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.54_PYAWYi2pt.png?updatedAt=1636901825769",
     title: "lalala",
     album: "lololo"
   }
