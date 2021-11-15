9ff7b87cff355ba636d649d63af4b0c68ccd88bb
Isela <isela.v.alarcon@gmail.com>
2021-11-15 19:04:41 +0100

    setting the card styles

--- src/components/elements/Card/Card.js
+++ src/components/elements/Card/Card.js
@@ -6,10 +6,10 @@ const Card = () => {
   return (
     <CardStyle>
       <CardImg
-        src="https://ik.imagekit.io/ppayaz/ppayaz-website/paypal-logo_2_38uvSe1MY.png?updatedAt=1634221510370"
+        src="https://ik.imagekit.io/ppayaz/react_challenge5/Captura_de_Pantalla_2021-11-14_a_la_s__15.54.33_2PgPk3EEt8.png?updatedAt=1636901869272"
         alt="track"
       />
-      <Title>Bizarrap, Anuel</Title>
+      <Title>Bizarrap, Anuel AA</Title>
       <Album>abc</Album>
     </CardStyle>
   );
--- src/components/elements/Card/Card.styles.js
+++ src/components/elements/Card/Card.styles.js
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 
-export const CardImg = styled.div`
+export const CardImg = styled.img`
+  display: flex;
+  position: relative;
   width: 200px;
   height: 200px;
 `;
 export const CardStyle = styled.div`
   width: 200px;
   height: auto;
-  border: 1px solid black;
 `;
 export const Title = styled.div`
-  font-size: 20px;
-  color: blue;
+  font-size: 14px;
+  color: #333;
+  font-family: helvetica;
 `;
 
 export const Album = styled.div`
+  margin-top: 2px;
   font-size: 15px;
-  color: #ccc;
+  color: #999;
 `;
